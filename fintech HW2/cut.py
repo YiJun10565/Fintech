05a736610083edcef339133b2dbd74899220b677
宋岩叡 <b06902106@ntu.edu.tw>
2019-04-12 00:19:20 +0800

    update

--- fintech HW2/cut.py
+++ fintech HW2/cut.py
@@ -9,12 +9,25 @@ fp = open('news.txt','r')
 list = fp.readline() 
 wfp = open('tags.txt', 'w')
 wfp.close()
+
 while list:
     list = list.strip('\n') 
-    content = open(list, 'rb').read() 
+    content = open(list, 'rb').read()
     tags = janal.extract_tags(content, 10)
+
+    fpn = open(list, 'r')
+    for(line, value) in enumerate(fpn):
+        if line == 1: 
+            name = fpn.readline()
+    print(name[6:9])
+    fpn.close()
+
     result = open('tags.txt', 'a')
+    result.write(name[6:9]+",")
     result.write(",".join(tags))
     result.write("\n")
     result.close()
+    
     list = fp.readline()
+
+fp.close()
